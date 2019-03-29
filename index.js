c9d48cf0a10683f316e7936171bccfe3026a2fa5
Anthony <Anthony@computers-MacBook-Pro.local>
2019-03-29 12:58:28 -0700

    MVP functionality finished.

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
     let topic = '';
     let limit = 25;
     let sortBy = "relevance"
     let storage = [];
     let result = "";
-    
-
-
-    
-
-      //grabs topic from input and triggers search
-    $(".topic-search").click(function() {
+    //grabs topic from input and triggers search
+    $(".topic-search").click(function () {
         console.log("topic", topic)
+        $(".submission-con").remove();
         let subject = $(".topic-form").val();
+        let sortBy = $(".sortby-dropdown").val();
+        let limit = $(".limit-dropdown").val();
+        console.log("sortBy", sortBy);
         console.log("topic", topic)
         $.ajax({
             url: `http://www.reddit.com/search.json?q=${subject}&sort=${sortBy}&limit=${limit}`,
-        }).then(function(data) {
+        }).then(function (data) {
             let results = data.data.children;
             let submission = "";
             // console.log(data.data.children);
-           for (var i = 0; i < results.length; i++) {
-            //    console.log(results[i].data);
-               console.log("thumbnail", results[i].data);
-               
-               if (results[i].data.thumbnail.slice(0, 4) === "http") {
-                submission = `<div class="submission-con">
-               <h3>${results[i].data.title}</h3>
-               <img src="${results[i].data.thumbnail}" height="${results[i].data.thumbnail_height}" width="${results[i].data.thumnail_width}"/>
+            for (var i = 0; i < results.length; i++) {
+                console.log("thumbnail", results[i].data);
+                let thread = results[i].data;
+                console.log("text", results[i].data.selftext);
+                if (thread.thumbnail.slice(0, 4) === "http") {
+                    submission = `<div class="submission-con">
+               <a href="${thread.url}">${thread.title}</a>
+               <img src="${thread.thumbnail}" height="${thread.thumbnail_height}" width="${thread.thumnail_width}"/>
+               <p>${thread.selftext}<p/>
                </div>`;
+                    $("#feed").append(submission);
+                } else {
+                    submission = `<div class="submission-con">
+                <a href="${thread.url}">${thread.title}</a>
+                <p>${thread.selftext}</p>
 
-               $("#feed").append(submission);
-               } else {
-                submission = `<div class="submission-con">
-                <h3>${results[i].data.title}</h3>
                 </div>`;
-                $("#feed").append(submission);
-               }
-               
-               
-           }
-        }).done(function(data) {
+                    $("#feed").append(submission);
+                }
+            }
+        }).done(function (data) {
             console.log("finished");
         })
     })
